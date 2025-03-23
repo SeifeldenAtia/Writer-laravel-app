c8676fc81ee24f0ea798188ae3b99f439f6abff3
Seifelden <siefeldenabualsoad@gmail.com>
2025-03-23 13:48:44 +0100

    feat: Color mode, header, footer, head tags and scropts added as reusable components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Writer</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-    <link rel="icon" href="{{ asset('logo.png') }}" type="image/x-icon">
-    <link rel="stylesheet" href="{{ asset('css/colorMode.css') }}">
+    @include('layout.head')
 </head>
 
 <body>
 
     @include('layout.colorMode')
-
-
-
     <div class="container">
         @include('layout.header')
         <div class="nav-scroller py-1 mb-3 border-bottom">
@@ -359,20 +352,9 @@
 
     </main>
 
-    <footer class="py-5 text-center text-body-secondary bg-body-tertiary">
-        <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a
-                href="https://twitter.com/mdo">@mdo</a>.</p>
-        <p class="mb-0">
-            <a href="#">Back to top</a>
-        </p>
-    </footer>
-    <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    <script src="https://getbootstrap.com/docs/5.3/assets/js/color-modes.js"></script>
+
+    @include('layout.footer')
+    @include('layout.scripts')
 </body>
 
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,27 +3,32 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('layout.head')
     <title>Posts</title>
 </head>
 
 <body>
 
-    <h1>
-        Posts
-    </h1>
-
-    <ul>
-        @foreach ($posts as $post)
-            <li>
-                <a href="{{ route('posts.show', $post->id) }}">
-                    {{ $post->title }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
-
+    @include('layout.colorMode')
+    <div class="container">
+        @include('layout.header')
+        <h1>
+            Posts
+        </h1>
+        <ul>
+            @foreach ($posts as $post)
+                <li>
+                    <a href="{{ route('posts.show', $post->id) }}">
+                        {{ $post->title }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    @include('layout.footer')
+    @include('layout.scripts')
 </body>
 
 </html>
